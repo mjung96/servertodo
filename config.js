30689ce2e7ed264ed55d09da281ab12c421856e3
Michael Jung <michaeljung1996@gmail.com>
2021-12-03 14:29:05 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://mjung:todo-app321@cluster0.ueja4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    SECRET_KEY: 'hellothereiamachickenfoot'
+};
